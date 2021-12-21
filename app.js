342a87b85a4ac18c9420e0bc4eae820f371bd44b
Oscar Menacho <58416396+Omr30@users.noreply.github.com>
2021-12-21 12:44:30 -0500

    Cambiar el nombre en package y agregando envios de prueba

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ const port = 3000;
 app.get('/', (req, res) => {
     // req es la request, la peticion
     // res es la respuesta
-    console.log(req);
-    res.send('Hello World!');
+    // console.log(req);
+    res.status(200).send('Hello World!');
 
 });
 
-app.post('/team/pokemons', () => {
-
+app.post('/team/pokemons', (req, res) => {
+    res.status(200).send('Hello World!');
 });
 
-app.get('/team', () => {
-
+app.get('/team', (req, res) => {
+    res.status(200).send('Hello World!');
 });
 
-app.delete('/team/pokemons/:pokeid', () => {
-
+app.delete('/team/pokemons/:pokeid', (req, res) => {
+    res.status(200).send('Hello World!');
 });
 
-app.put('/team', () => {
-
+app.put('/team', (req, res) => {
+    res.status(200).send('Hello World!');
 });
 
 app.listen(port, () => {
